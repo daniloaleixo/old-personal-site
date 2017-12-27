9d8ea0af0f167de086380a59655238a29ef108a4
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-12-27 21:56:33 -0200

    Desinstala Mailgun
    - Vou tentar com o Sendgrid

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -28,10 +33,21 @@ app.get('/cool', function(request, response) {
 app.post('/email', function(request, response) {
 	console.log('O que vei', request.body);
 
+	const email = request.body.email;
+	const message = request.body.message;
+
+	const msg = {
+	  to: 'danilo_aleixo@hotmail.com',
+	  from: email,
+	  subject: 'Danilo Aleixo | Personal Website',
+	  text: message
+	};
+	sgMail.send(msg);
+
 	response.sendStatus(200);
+
 });
 
 app.listen(app.get('port'), function() {
-	console.log(process.env.MAILGUN);
     console.log('Node app is running on port', app.get('port'));
 });
