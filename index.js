39ff84cf5e34ae0e188bf1dfdbc3a9e6fd7ccb20
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-12-27 21:16:48 -0200

    FAzendo a rota para mandar email

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var config = require('./config.js');
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -16,6 +21,14 @@ app.get('/', function(request, response) {
 
 app.get('/cool', function(request, response) {
   response.send(cool());
+});
+
+
+// Email engine
+app.post('/email', function(request, response) {
+	console.log('O que vei', request.body);
+
+	response.sendStatus(200);
 });
 
 app.listen(app.get('port'), function() {
