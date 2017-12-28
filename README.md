402650ea12995cc156c42db1d8e2f5c01c96a6c7
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-12-27 23:07:37 -0200

    Estou fazendo o index.html redirecionar para o heroku
    - Pois agora o heroku que tem o SMTP e assim preciso acessar ele para conseguir enviar os emails corretamente
    - Pretendo manter o index.html, para manter o endereço do github

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# daniloaleixo.github.io-backend
-
-Just a simple backend with a SMTP server
-
+# daniloaleixo.github.io
+Personal portfolio implemented with a simple backend with a SMTP server
 
 ### Client
 Forked from: https://github.com/RyanFitzgerald/devportfolio-template
+
+
 
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/main.html'));
 });
 
 app.use(bodyParser.json()); // support json encoded bodies
